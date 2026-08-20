590cd081e4a85f738d4d6658da877c76349d96d6
KeithWass <keithwass@hotmail.com>
2026-08-20 10:25:20 +0100

    Add initial React login functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
+
   async function checkApi() {
     const response = await fetch(
       "https://movie-journal-production.up.railway.app/healthcheck",
@@ -9,9 +14,44 @@ function App() {
     console.log(data);
   }
 
+  async function handleLogin() {
+    const response = await fetch(
+      "https://movie-journal-production.up.railway.app/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      },
+    );
+    const data = await response.json();
+    setToken(data.token);
+    console.log(data);
+  }
+
+  function handleRegister() {}
   return (
     <>
       <button onClick={checkApi}>Check API</button>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+      </form>
     </>
   );
 }
