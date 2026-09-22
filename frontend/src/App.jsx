7e23b611c589299010996e59667076e8721d613e
KeithWass <keithwass@hotmail.com>
2026-09-22 17:19:50 +0100

    Worked on login and added MovieList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 
 function App() {
   const [token, setToken] = useState("");
+  const [movies, setMovies] = useState([]);
 
   async function checkApi() {
     const response = await fetch(
@@ -13,10 +14,36 @@ function App() {
     console.log(data);
   }
 
+  async function getMovies() {
+    const response = await fetch(
+      "https://movie-journal-o7uq.onrender.com/movies",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    const data = await response.json();
+    setMovies(data);
+  }
+
   return (
     <>
       <button onClick={checkApi}>Check API</button>
-      <Login setToken={setToken} />
+
+      {!token && <Login setToken={setToken} />}
+
+      {token && (
+        <div>
+          <button onClick={getMovies}>Get Movies</button>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <p>{movie.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
